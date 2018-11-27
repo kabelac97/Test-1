1862aa0d388b9a77328576e6d82a2ece02d588fd
Carly Kabelac <carly_kabelac@brown.edu>
2018-11-27 18:39:11 -0500

    Practice

---
+++ file1.jl
@@ -0,0 +1,19 @@
+
+# say hello to user 
+println("hello, world")
+print("How are you? ") #print question 
+print("Gr8! \n")
+
+#= 
+comment
+commmmment 
+commmmmmmmment 
+=# 
+
+const greeting = "Hello"
+name1 = "World"
+name2 = "User"
+firstname = ARGS[1]
+lastname = ARGS[2]
+print("")
+println("Hello $(firstname) $(lastname)!")
